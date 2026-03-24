d3c25da6276bc7923e0d0bca1352dacaa3ea1ae4
hqers <coqers@gmail.com>
2026-03-24 15:56:33 +0700

    Versi 1.0.2
    Perbaikan pada pipeline

--- mixclust/__init__.py
+++ mixclust/__init__.py
@@ -15,7 +15,7 @@ Paper:
     AUFS-Samba: https://doi.org/10.1109/ACCESS.2026.3653624
 """
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __author__  = "Hasta Pratama"
 __email__   = "33220015@std.stei.itb.ac.id"
 
